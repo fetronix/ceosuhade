7e19edada087d6a23275fd43b1f4fbdcd451eeb1
fetronix <fidelism99@gmail.com>
2024-07-04 03:50:28 +0300

    edits

--- account/urls.py
+++ account/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name= 'index'),
     path('login/', views.login_view, name='login_view'),
-    path('logout/', views.LogoutView, name='logout'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
     path('register/', views.register, name='register'),
     path('adminpage/', views.admin, name='adminpage'),
     path('student/', views.student, name='student'),
